9d4e5d4958af811a6b1f881b3df3feb0ab21b2ba
Sven Werner <vegascowboy@web.de>
2018-06-05 17:15:14 +0200

    Task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :tasks
+  get    "tasks",          to: "tasks#index"
+
+  get    "tasks/new",      to: "tasks#new"
+  post   "tasks",          to: "tasks#create"
+
+  # NB: The `show` route needs to be *after* `new` route.
+  get    "tasks/:id",      to: "tasks#show", as: :task
+
+  get    "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+
+  patch  "tasks/:id",      to: "tasks#update"
+
+  delete "tasks/:id",      to: "tasks#destroy", as: :task_destroy
 end
